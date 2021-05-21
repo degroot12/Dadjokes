04ac7b7bd13106f4fa1b9cc03058c9bffd91d7e2
Thomas de Groot <degroot12@gmail.com>
2021-05-21 12:04:11 +0200

    firs nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2> 
+      Welcome to the best corny dad jokes app
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Welcome to dadJokes',
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "best place for corny dad jokes"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
